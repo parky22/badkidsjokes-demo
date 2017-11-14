0a5af6a7f1147a2382cf299430c92b772b59f477
Yoo-Nah <ypark2270@gmail.com>
2017-11-14 18:08:20 -0500

    first commit

---
+++ backend/jokes.js
@@ -0,0 +1,52 @@
+const jokes = [
+  {
+    q: "WHY BABIES DO NOT TALK?",
+    a: "BECAUSE THEY HAVE INVISIBLE YAM IN THEIR MOUTH."
+  },
+  {
+    q: "What’s the difference between a ps4 and a Xbox One.",
+    a: "The name."
+  },
+  {
+    q: "did you hear about the goblin with no head",
+    a: "hes ok now"
+  },
+  {
+    q: "why was 10 afraid of 20",
+    a: "because 20 is 10 times more than 10."
+  },
+  {
+    q: "what do you do when a pigeon poops on your nose",
+    a: "poop on the pigeons nose and never look back"
+  },
+  {
+    q: "what do you call a computer with no smartness",
+    a: "a wire"
+  },
+  {
+    q: "what did the chiken do when he crossed the road.",
+    a: "pooed on the road"
+  },
+  {
+    q: "​what did the mitten say to the hat?",
+    a: "You go up there and i stay down here"
+  },
+  {
+    q: "why crow is black?",
+    a: "because it has been deep fried"
+  },
+  {
+    q: "the man says would you like some cash",
+    a: "and the other man says no i want PEANUTS"
+  },
+  {
+    q: "​why does the crow laugh",
+    a: "beacause they are mad ha ha ha I am also mad"
+  },
+  {
+    q: "​WHAT DO U CALL A FREAK HORSE",
+    a: "JESSICA"
+  }
+]
+
+export default jokes
